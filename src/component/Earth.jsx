6728ed823b831db30277cb3ec12426ba408ab02f
YinHk <>
2022-07-16 23:30:13 -0400

    feat: add changes

--- src/component/Earth.jsx
+++ src/component/Earth.jsx
@@ -93,7 +93,14 @@ export default function Earth(props) {
         });
 
         //Set the default atmosphere style
-        map.on('load', () => map.setFog({}) );
+        map.on('load', () => map.setFog({
+            'range': [-1, 2],
+            'horizon-blend': 0.,
+            'color': '#242B4B',
+            'high-color': 'rgba(0, 0, 0, .9)',
+            'space-color': '#0B1026',
+            'star-intensity': 1.2
+        }));
 
         //detect pointer move and corresponding position
         //map.on('mousemove', (e) => { console.log(e.lngLat)});
